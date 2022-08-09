d68f1c4d405ed8d5537a27c4337a27064762d119
Regina <reginabb68@gmail.com>
2022-08-09 18:40:23 +0800

    new

--- src/components/MyWork/ProjectsGallery.js
+++ src/components/MyWork/ProjectsGallery.js
@@ -6,16 +6,19 @@ function ProjectsGallery() {
     <div className="MyWork">
       <div>
         <div>
-          <h1>To-Do List</h1>
+          <h1>Full Stack Project</h1>
           <p className="paragraph">
-            This is a React project styled with TailwindCSS, it includes the
-            basic function of adding and editing, other than that it has another
-            section for finished tasks, and it lets you examine what items you
-            have been done before and you can also delete it afterwards! I used
-            json-server as a mock API to store the data for the to-do list. The
-            json-server is being deployed on Heroku.
-            <a href="/my-work/to-do">
-              <button className="buttonNeonYellow">
+            This is a completed version for to-do list, I used ReactJS, NodeJS,
+            Express, MySQL to build a basic login/register system, It includes
+            registering users, inserting data into a DataBase, and reading the
+            data to allow a login. Once users register/login in their account,
+            they are able to read and create their own data on their the to-do
+            list.
+            <a
+              href="https://github.com/rerebebe/new-mysql-0807"
+              target="rel=noopener"
+            >
+              <button className="buttonNeonOrange">
                 Check this out
                 <IoIosArrowRoundForward size="1.5rem" margin="auto" />
               </button>
@@ -23,9 +26,15 @@ function ProjectsGallery() {
           </p>
         </div>
         <div className="ImgWrap">
-          <img src="/to-do.png" alt="To Do List" width="600" height="500" />
+          <img
+            src="/MySQL.png"
+            alt="MySQL login ToDoList"
+            width="640"
+            height="420"
+          />
         </div>
       </div>
+
       <div className="div2n4">
         <div className="ImgWrap">
           <img src="/hotelPage.png" alt="hotel page" width="600" height="500" />
@@ -57,7 +66,7 @@ function ProjectsGallery() {
             comments in the end based on your score of the quiz.
             <a href="/my-work/Quiz">
               <button className="buttonNeonYellow">
-                Check this out{" "}
+                Check this out
                 <IoIosArrowRoundForward size="1.5rem" margin="auto" />
               </button>
             </a>
@@ -83,42 +92,33 @@ function ProjectsGallery() {
             Tailwind, and shrtcode API.
             <a href="/my-work/shortner">
               <button className="buttonNeonOrange">
-                Check this out{" "}
+                Check this out
                 <IoIosArrowRoundForward size="1.5rem" margin="auto" />
               </button>
-            </a>{" "}
-            ` `
+            </a>
           </p>
         </div>
       </div>
       <div>
         <div>
-          <h1>Full Stack Project</h1>
+          <h1>To-Do List</h1>
           <p className="paragraph">
-            This is a completed version for to-do list, I used ReactJS, NodeJS,
-            Express, MySQL to build a basic login/register system, It includes
-            registering users, inserting data into a DataBase, and reading the
-            data to allow a login. Once users register/login in their account,
-            they are able to read and create their own data on their the to-do
-            list.
-            <a
-              href="https://github.com/rerebebe/new-mysql-0807"
-              target="rel=noopener"
-            >
-              <button className="buttonNeonOrange">
-                Check this out{" "}
+            This is a React project styled with TailwindCSS, it includes the
+            basic function of adding and editing, other than that it has another
+            section for finished tasks, and it lets you examine what items you
+            have been done before and you can also delete it afterwards! I used
+            json-server as a mock API to store the data for the to-do list. The
+            json-server is being deployed on Heroku.
+            <a href="/my-work/to-do">
+              <button className="buttonNeonYellow">
+                Check this out
                 <IoIosArrowRoundForward size="1.5rem" margin="auto" />
               </button>
             </a>
           </p>
         </div>
         <div className="ImgWrap">
-          <img
-            src="/MySQL.png"
-            alt="MySQL login ToDoList"
-            width="640"
-            height="420"
-          />
+          <img src="/to-do.png" alt="To Do List" width="600" height="500" />
         </div>
       </div>
     </div>
